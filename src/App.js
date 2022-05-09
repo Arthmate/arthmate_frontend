fab9133dfc7d7e38c8ad20d0cd631345f034241a
Gaurav <gsingh@analyticsfoxsoftwares.com>
2022-05-09 18:23:02 +0530

    Login Screen Done

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Login from './components/login/Login';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from './components/dashboard/Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={<Login />}
+        />
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
