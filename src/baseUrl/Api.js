e2dae5b12f4a6e27bc7014438750ef001b5c3d7c
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2022-06-16 15:07:35 +0530

    fix(upload Summary):change in validation result

--- src/baseUrl/Api.js
+++ src/baseUrl/Api.js
@@ -1,8 +1,10 @@
 
 export const environment = {
-  // BaseUrl: 'http://k8s-default-devingre-6cc6540f26-504905718.ap-south-1.elb.amazonaws.com/lms-portal-service/', -QA
-  // BaseUrlToUpload: "http://13.233.251.173:2022/api/kuliza/v1/", //-QA
+  BaseUrl: "http://k8s-default-devingre-6cc6540f26-504905718.ap-south-1.elb.amazonaws.com/lms-portal-service/", //-QA
+  BaseUrlToUpload: "http://13.233.251.173:2022/api/kuliza/v1/", //-QA
 
-  BaseUrl: 'https://uat-lms2.arthmate.com/lms-portal-service/', //-UAT
-  BaseUrlToUpload : "http://3.111.115.55:2022/api/kuliza/v1/", //-UAT
+  // BaseUrlToUpload: "http://localhost:2023/api/kuliza/v1/", //-LOCAL
+
+  // BaseUrl: "https://uat-lms2.arthmate.com/lms-portal-service/", //-UAT
+  // BaseUrlToUpload: "http://3.111.115.55:2022/api/kuliza/v1/", //-UAT
 };
--- src/components/uploadSummary/UploadSummary.js
+++ src/components/uploadSummary/UploadSummary.js
@@ -1,80 +1,203 @@
-import React, { useState } from 'react';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { Divider, Button, Typography, MenuItem } from '@mui/material';
-import axios from 'axios';
-import { environment } from '../../baseUrl/Api';
-import { Details } from '@mui/icons-material';
+import React, { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Divider, Button, Typography, MenuItem } from "@mui/material";
+import axios from "axios";
+import { environment } from "../../baseUrl/Api";
+import { Details } from "@mui/icons-material";
 
-function UploadSummary({ uploadSummary, alert, uuId, setAlert, handleClick, status }) {
-    const { validationFailures, uploadFailures } = uploadSummary;
+function UploadSummary({
+  uploadSummary,
+  alert,
+  uuId,
+  setAlert,
+  handleClick,
+  status,
+}) {
+  const { validationFailures, failures, successes } =
+    uploadSummary;
 
-    const handleDownloadSummary = async () => {
-        await axios.get(environment.BaseUrlToUpload + `sheetUploadStatusDownload/${uuId}`, {
-            responseType: 'blob',
-        })
-            .then(response => {
-                const url = window.URL.createObjectURL(new Blob([response.data]));
-                const link = document.createElement('a');
-                link.href = url;
-                link.setAttribute('download', 'SummaryReport.xlsx');
-                document.body.appendChild(link);
-                link.click();
-            })
-            .catch(error => {
-                handleClick("Something went wrong");
-            });
-    }
+  const handleDownloadSummary = async () => {
+    await axios
+      .get(environment.BaseUrlToUpload + `sheetUploadStatusDownload/${uuId}`, {
+        responseType: "blob",
+      })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "SummaryReport.xlsx");
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((error) => {
+        handleClick("Something went wrong");
+      });
+  };
 
-    return (
-        <>
-            <Dialog
-                open={alert}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-                maxWidth="md"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {"Upload Summary Report :-"}
-                </DialogTitle>
-                <Divider></Divider>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        <Typography><span><b>Total Records</b></span> - {uploadSummary.totalRecords ? uploadSummary.totalRecords : "0"}</Typography>
-                        <Typography><span><b>Validation Passed</b></span> - {uploadSummary.validationPassed ? uploadSummary.validationPassed : "0"}</Typography>
-                        <Typography><span><b>Success</b></span> - {uploadSummary.successfullyUploaded ? uploadSummary.successfullyUploaded : "0"}</Typography>
-                        <Typography><span><b>Exception</b></span> - {uploadSummary.failedToUpload ? uploadSummary.failedToUpload : "0"}</Typography>
-                        {uuId ? (<Typography><span><b>Click Here To Download</b></span> - <Button variant="text" onClick={handleDownloadSummary}>Summary Report</Button></Typography>) : ''}
-                        {uploadFailures?.length > 0 ? <Divider sx={{ m: 2 }}></Divider> : ""}
-                        {uploadFailures?.map((fail) => (
-                            <>
-                                <Typography><span><b>PAN Number</b></span> - {fail.personalPanNumber ? fail.personalPanNumber : ""}</Typography>
-                                <Typography sx={{ mb: 2 }}><span><b>Process Failure</b></span> - {fail.processFailure ? fail[fail.processFailure] : ""}</Typography>
-                            </>
-                        ))}
-                        {validationFailures?.length > 0 ? <Divider sx={{ m: 2 }}></Divider> : ""}
-                        {validationFailures?.map((valid) => {
-                            let message = [];
-                            valid?.details?.map((data) => (
-                                message.push(data.message)
-                            ))
-                            return (<>
-                                <Typography sx={{ mb: 2 }}><span><b>{valid._original.personalPanNumber ? valid._original.personalPanNumber : valid._original.partnerLoanId}</b></span> :- <span>{message.join()}</span></Typography>
-                            </>)
-                        })}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => setAlert(false)} autoFocus>
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </>
-    )
+  return (
+    <>
+      <Dialog
+        open={alert}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        maxWidth="md"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Upload Summary Report :-"}
+        </DialogTitle>
+        <Divider></Divider>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <Typography>
+              <span>
+                <b>Total Records</b>
+              </span>{" "}
+              - {uploadSummary.totalRecords ? uploadSummary.totalRecords : "0"}
+            </Typography>
+            <Typography>
+              <span>
+                <b>Validation Passed </b>(Data Format and Unique Partner Id)
+              </span>{" "}
+              -{" "}
+              {uploadSummary.validationPassed
+                ? uploadSummary.validationPassed
+                : "0"}
+            </Typography>
+            <Typography>
+              <span>
+                <b>Success</b>
+              </span>{" "}
+              -{" "}
+              {uploadSummary.fullyProcessed
+                ? uploadSummary.fullyProcessed
+                : "0"}
+            </Typography>
+            <Typography>
+              <span>
+                <b>Exception</b>
+              </span>{" "}
+              - {uploadSummary.totalRecords - uploadSummary.fullyProcessed}
+            </Typography>
+            {uuId ? (
+              <Typography>
+                <span>
+                  <b>Click Here To Download</b>
+                </span>{" "}
+                -{" "}
+                <Button variant="text" onClick={handleDownloadSummary}>
+                  Summary Report
+                </Button>
+              </Typography>
+            ) : (
+              ""
+            )}
+            {failures?.length > 0 ? <Divider sx={{ m: 2 }}></Divider> : ""}
+            {failures?.map((fail) => (
+              <>
+                <Typography>
+                  <span>
+                    <b>Partner Loan Id</b>
+                  </span>{" "}
+                  - {fail.partnerLoanId ? fail.partnerLoanId : ""}
+                </Typography>
+                <Typography sx={{ mb: 2 }}>
+                  <span>
+                    <b>Process Failure</b>
+                  </span>{" "}
+                  - {fail.processFailure ? fail[fail.processFailure] : ""}
+                </Typography>
+              </>
+            ))}
+            {validationFailures?.length > 0 ? (
+              <Divider sx={{ m: 2 }}></Divider>
+            ) : (
+              ""
+            )}
+            {validationFailures?.map((item) => {
+              console.log(item);
+              if (item.validationResponse) {
+                return (
+                  <>
+                    <Typography sx={{ mb: 2 }}>
+                      <span>
+                        <b>{item.partnerLoanId}</b>
+                      </span>{" "}
+                      :- <span>{item.validationResponse}</span>
+                    </Typography>
+                  </>
+                );
+              } else {
+                let valid = item;
+                let message = [];
+                valid?.details?.map((data) => message.push(data.message));
+                return (
+                  <>
+                    <Typography sx={{ mb: 2 }}>
+                      <span>
+                        <b>{item._original.partnerLoanId}</b>
+                      </span>{" "}
+                      :- <span>{message.join()}</span>
+                    </Typography>
+                  </>
+                );
+              }
+            })}
+            {successes?.length > 0 ? <Divider sx={{ m: 2 }}></Divider> : ""}
+            {successes?.map((success) => (
+              <>
+                <Typography>
+                  <span>
+                    <b>Partner Loan Id</b>
+                  </span>{" "}
+                  - {success.partnerLoanId ? success.partnerLoanId : ""}
+                </Typography>
+                <Typography sx={{ mb: 2 }}>
+                  <span>
+                    <b>Last Stage</b>
+                  </span>{" "}
+                  -{" "}
+                  {success.loanDisbursementResponse
+                    ? "Loan Approved Successfully - " +
+                      JSON.parse(success.loanCreationResponse).data
+                        .outputVariables.loanNumber +
+                      "(Loan Number)"
+                    : success.loanApproveResponse
+                    ? "Loan Approved Successfully - " +
+                      JSON.parse(success.loanCreationResponse).data
+                        .outputVariables.loanNumber +
+                      "(Loan Number)"
+                    : success.loanCreationResponse
+                    ? "Loan Created Successfully - " +
+                      JSON.parse(success.loanCreationResponse).data
+                        .outputVariables.loanNumber +
+                      "(Loan Number)"
+                    : success.userCreationResponse
+                    ? "Client Created Successfully - " +
+                      JSON.parse(success.userCreationResponse).data
+                        .outputVariables.clientNumber +
+                      "(Client Number)"
+                    : success.kulizaBREResponse
+                    ? "BRE Passed Successfully - " + success.kulizaBREResponse
+                    : success.validationResponse
+                    ? success.validationResponse
+                    : success[success.processFailure]}
+                </Typography>
+              </>
+            ))}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setAlert(false)} autoFocus>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
 
-export default UploadSummary
+export default UploadSummary;
